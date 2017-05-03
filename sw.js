409295f77a91c9082af977597cfb145f2fc479a4
submerge <277275741@qq.com>
2017-05-03 17:07:15 +0800

    caches

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
+console.log(caches);
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('mysite-static-v1').then(function(cache) {
+      console.log(cache);
       return cache.addAll([
         '/sw/css/index.css',
         '/sw/js/index.js'
