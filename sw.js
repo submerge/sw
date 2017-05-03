6b7a323f8c05f05989a02746f5664b25c5a4be41
submerge <277275741@qq.com>
2017-05-03 17:19:58 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,3 +10,20 @@ self.addEventListener('install', function(event) {
     })
   );
 });
+
+
+self.addEventListener('fetch', function(event) {
+  console.log('fetch');
+  console.log(event.request);
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
