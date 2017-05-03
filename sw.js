c2ae929449691afbc675f43ba0cd06e91e6e5ca1
leike [雷轲] <leike@xiaomi.com>
2017-05-03 17:00:12 +0800

    code save

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('mysite-static-v1').then(function(cache) {
       return cache.addAll([
-        '/css/index.css',
-        '/js/index.js'
+        '/sw/css/index.css',
+        '/sw/js/index.js'
       ]);
     })
   );
