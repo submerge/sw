bbf524083589d35b51facaa93ddff49ab645cd19
leike <leike@meituan.com>
2017-12-24 17:21:45 +0800

    code save

--- ww.js
+++ ww.js
@@ -13,6 +13,7 @@ self.addEventListener('install', function(event) {
 
 
 self.addEventListener('fetch', function(event) {
+  console.log(event.request.url)
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
