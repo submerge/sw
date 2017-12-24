e7b3b88a6d15657111a6c849631025a32de76ae8
leike <leike@meituan.com>
2017-12-24 16:50:10 +0800

    code save

--- ww.js
+++ ww.js
@@ -3,7 +3,7 @@ console.log('ww')
 // 安装
 self.addEventListener('install', function(event) {
     event.waitUntil(
-      caches.open('mysite-static-v1').then(function(cache) {
+      caches.open('mysite-static-v2').then(function(cache) {
         return cache.addAll([
           './css/index.css'
         ]);
