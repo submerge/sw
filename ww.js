069db40738cbf23243e9eeaaffe23cd62fc61ede
leike <leike@meituan.com>
2017-12-24 16:25:57 +0800

    code save

--- ww.js
+++ ww.js
@@ -1,1 +1,12 @@
 console.log('ww')
+
+// 安装
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('mysite-static-v1').then(function(cache) {
+        return cache.addAll([
+          './css/index.css'
+        ]);
+      })
+    );
+  });
