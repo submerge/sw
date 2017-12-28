97db91bac0e5d71309e3db2d5e0f5b32e69f5eb3
leike <leike@meituan.com>
2017-12-28 23:28:48 +0800

    code save

--- dist/sw.js
+++ dist/sw.js
@@ -1,18 +1,26 @@
+// service work 本身在单独的一个线程中运行，
+// 不能访问DOM
+
+// service work中只能进行异步操作，不能进行通过操作
+// 比如 localStorage.setItem操作为同步，就不能在
+// service work 中使用
 
 // importScripts('./path-to-regexp.js');
 
-console.log('test3 test4');
+// console.log('test3 test4');
 // 需要缓存的文件类型
+// 只允许缓存js, css, png等类型资源
 const FILE_LISTS = ['js','css','png'];
 
 //  缓存版本
-const CACHE_VERSION = 1;
+const CACHE_VERSION = '1';
 const CURRENT_CACHES = {
     prefetch: 'prefetch-cache-v' + CACHE_VERSION
 };
 
 
-// 安装
+// 安装阶段，将首屏需要的资源进行加载并缓存
+// 也可以将用户可能会使用到的资源进行预加载
 self.addEventListener('install', function(event) {
    // 缓存指定的文件
     const urlsToPrefetch = [
@@ -21,7 +29,9 @@ self.addEventListener('install', function(event) {
   
     event.waitUntil(
       caches.open(CURRENT_CACHES.prefetch).then(function(cache) {
+        // 遍历需要缓存的资源list
         var cachePromises = urlsToPrefetch.map(function(urlToPrefetch) {
+          // 拼接资源url
           var url = new URL(urlToPrefetch,location.origin); // 拼路径
   
           console.log('now send the request to' + url);
@@ -49,12 +59,53 @@ self.addEventListener('install', function(event) {
   });
 
 
-var goSaving = function(url){
-   for(var file of FILE_LISTS){
-        if(url.endsWith(file)) return true;
-    }
-    return false;
-}
+// 调用cache API 缓存的资源除非手动删除，否贼一只在设备硬盘中
+// activate 事件中，手工清楚之前版本的缓存
+self.addEventListener('activate', function(event) {
+  // var cacheWhitelist = ['v1'];
+  event.waitUntil(
+  // 遍历 caches 里所有缓存的 keys 值
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          // if (cacheWhitelist.includes(cacheName)) {
+          if (cacheName != CURRENT_CACHES.prefetch) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+
+// 此处fetch事件比较关键,比较牛逼
+// 拦截应用中所有的http请求
+// 判断如果请求的是js, css, png且缓存中有，则直接从缓存中返回
+// 否则，正常向服务端发送http请求
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+  caches.match(event.request).then(function(resp) {
+      // 如果缓存中有，则直接返回
+      // 否则从服务器拉取，并更新缓存
+      return resp || fetch(event.request).then(function(response) {
+          console.log('save file:' + response.url);
+
+           if(!checkFile(event.request)) {
+              return response;
+           }
+          // 需要缓存,则将资源放到 caches Object 中
+          return caches.open(CURRENT_CACHES.prefetch).then(function(cache) {
+            // 此处注意: response.clone()
+            // 因为一个流只能被使用一次  
+            cache.put(event.request, response.clone());
+              return response;
+          });
+      });
+  }));
+});
+
+
 
 // 判断文件是否需要被缓存
 function checkFile(request){
@@ -66,26 +117,12 @@ function checkFile(request){
     return !!(goSaving(url) && method === 'get'); // 只缓存get请求的静态资源
 }
 
-
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-    caches.match(event.request).then(function(resp) {
-        // 如果缓存中国年有，则直接返回
-        // 否则从服务器拉取，并更新缓存
-        return resp || fetch(event.request).then(function(response) {
-            console.log('save file:' + response.url);
-
-             if(!checkFile(event.request)) {
-                return response;
-             }
-            // 需要缓存,则将资源放到 caches Object 中
-            return caches.open(CURRENT_CACHES.prefetch).then(function(cache) {
-                cache.put(event.request, response.clone());
-                return response;
-            });
-        });
-    }));
-});
+var goSaving = function(url){
+  for(var file of FILE_LISTS){
+       if(url.endsWith(file)) return true;
+   }
+   return false;
+}
 
 
 // document 文件懒更新
@@ -108,7 +145,6 @@ self.addEventListener('message',event =>{
 
 
 
-/*
 // push
 // 在 SW 中使用
 function sendNote(){
@@ -136,6 +172,7 @@ function sendNote(){
 
 // sendNote();
 
+
 self.addEventListener('message',event =>{
   // test send note
   sendNote();
@@ -143,6 +180,10 @@ self.addEventListener('message',event =>{
   console.log("receive message" + event.data);
 });
 
+
+
+
+/*
 self.addEventListener('notificationclick', function(event) {
   var messageId = event.notification.data;
   event.notification.close();
@@ -167,18 +208,3 @@ function focusOpen(){
   })
 }
 */
-
-
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.open('mysite-dynamic').then(function(cache) {
-        return cache.match(event.request).then(function(response) {
-          var fetchPromise = fetch(event.request).then(function(networkResponse) {
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
-          })
-          return response || fetchPromise;
-        })
-      })
-    );
-});
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var path = require('path');
+var crypto = require('crypto');
+var fs = require('fs');
 const COPY = require('copy-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -8,6 +10,38 @@ const Manifest = path.resolve(__dirname, './manifest.json');
 const Path_To_Reg = path.resolve(__dirname, './path-to-regexp.js');
 
 const DIST = path.resolve(__dirname, './dist/');
+
+/*
+var getMd5 = function (filePath) {
+    var rs = fs.createReadStream(filePath);
+    var hash = crypto.createHash('md5');
+    
+    rs.on('data', hash.update.bind(hash));
+    
+    rs.on('end', function () {
+      // console.log(hash.digest('hex'));
+      var md5 = hash.digest('hex')
+      replaceVersion(md5)
+    });
+}
+*/
+function replaceVersion () {
+    fs.readFile("./sw.js",'utf-8',function(err,data){  
+        if(err){  
+            console.log("error");  
+        }else{  
+            // console.log(data);
+            var hash = crypto.createHash('md5');
+            hash.update(data)
+            var md5 = hash.digest('hex')
+            // var data = data.replace('${CACHE_VERSION}', '1')
+            console.log(data)
+            fs.writeFile('./sw.js', data, () => function () {
+                console.log(data)
+            })
+        }
+    });
+}
 
 module.exports = {
     entry: './js/index.js',
